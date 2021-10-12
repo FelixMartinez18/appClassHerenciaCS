91d5be12b1557929a1f4bdfc1d3af66e14acad92
derek <derek@FELIXMARTINEZ>
2021-10-12 09:25:12 -0500

    Agregar archivos de proyecto.

---
+++ appClassHerenciaCS/Punto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClassHerenciaCS
+{
+    class Punto
+    {
+        private double x;
+
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+
+        public Punto()
+        {
+            x = 0;
+        }
+        public Punto(double x)
+        {
+            this.x = x;
+        }
+        public double Distancia(Punto p)
+        {
+            double d = 0;
+            d = x - p.x  //valor absoluto 
+            return d;
+        }
+        public override string ToString()
+        {
+            return "X=" + x;
+        }
+    }
+}
---
+++ appClassHerenciaCS/Punto3D.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appClassHerenciaCS
+{
+    class Punto3D : Punto2D
+    {
+        private double z;
+        public double Z
+        {
+        get 
+        {
+        return z; 
+        }
+        set 
+        {
+        z = value; 
+        }
+        private double distance;
+            
+        public double Distance
+        {   
+        
+        get 
+        {
+        return distance; 
+        }
+        set
+        {
+        distance = value;
+        }
+        public Punto3D() : base()
+        {
+        z = 0;
+        }
+        public Punto3D(double x, double y, double z) : base(x,y)
+        {
+        this.z = z;
+        }
+        public double DistanciaXY(double x, double Y)
+        {
+        double d = x - Y;
+        distance = d;
+        return d;
+        }
+        public override string ToString()
+        {
+        return base.ToString(); + " Z=" + z "\n distancia X-Y=" + distance;
+
+        }
+        // no lo termine pero es a lo que pude llegar 
+    }
+}
